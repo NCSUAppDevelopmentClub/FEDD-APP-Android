ec12b1208e4a0734533e85f4de673da0833aea6f
Duncan Page <dmpage318@gmail.com>
2016-11-10 08:47:13 -0500

    fixed build errors

--- app/src/main/java/com/ncsuappdev/feddapp/Leaderboard.java
+++ app/src/main/java/com/ncsuappdev/feddapp/Leaderboard.java
@@ -2,30 +2,20 @@ package com.ncsuappdev.feddapp;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-
-import java.util.List;
 
 public class Leaderboard extends AppCompatActivity {
 
     public static class LeaderboardAdapter extends BaseAdapter {
-        public LeaderboardAdapter(Context context, int textViewResourceId) {
-            super(context, textViewResourceId);
-        }
+        Context context;
 
-        public LeaderboardAdapter(Context context, int resource, List<Leaderboard> items) {
-            super(context, resource, items);
+        public LeaderboardAdapter(Context context) {
+            this.context = context;
         }
 
         public int getCount(){
@@ -57,17 +47,8 @@ public class Leaderboard extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        ListView list;
-        list = (ListView) findViewById(R.id.list);
-        list.setAdapter(new LeaderboardAdapter(this, R.layout.content_leaderboard, ));
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setAdapter(new LeaderboardAdapter(this));
     }
 
 }
