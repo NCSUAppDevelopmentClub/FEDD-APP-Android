cb34080fef25adc206a8aaf22f4529ee521a4f95
Thomas Barnette <tvbarnet@ncsu.edu>
2016-11-09 21:14:15 -0500

    Added some leaderboard stuff

--- app/src/main/java/com/ncsuappdev/feddapp/Leaderboard.java
+++ app/src/main/java/com/ncsuappdev/feddapp/Leaderboard.java
@@ -1,13 +1,53 @@
 package com.ncsuappdev.feddapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class Leaderboard extends AppCompatActivity {
+
+    public static class LeaderboardAdapter extends BaseAdapter {
+        public LeaderboardAdapter(Context context, int textViewResourceId) {
+            super(context, textViewResourceId);
+        }
+
+        public LeaderboardAdapter(Context context, int resource, List<Leaderboard> items) {
+            super(context, resource, items);
+        }
+
+        public int getCount(){
+            return LeaderboardData.getInstance().scores.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return LeaderboardData.getInstance().scores[i];
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +65,9 @@ public class Leaderboard extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        ListView list;
+        list = (ListView) findViewById(R.id.list);
+        list.setAdapter(new LeaderboardAdapter(this, R.layout.content_leaderboard, ));
     }
 
 }
