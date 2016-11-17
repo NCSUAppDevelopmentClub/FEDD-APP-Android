f5689308adf2d46137ca2b05c11e9705a832d7a4
Thomas Barnette <tvbarnet@ncsu.edu>
2016-11-17 17:51:49 -0500

    Merged and started the edit score activity

--- app/src/main/java/com/ncsuappdev/feddapp/ScoreEdit.java
+++ app/src/main/java/com/ncsuappdev/feddapp/ScoreEdit.java
@@ -1,15 +1,24 @@
 package com.ncsuappdev.feddapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ScoreEdit extends AppCompatActivity {
 
-    ArrayList<String> labels = new ArrayList<String>();
-    ArrayList<Integer> scores = new ArrayList<Integer>();
+    public static class Entry{
+        int max;
+        String label;
+        int value;
+    }
+    ArrayList<Entry> entries = new ArrayList<Entry>();
 
     String project;
     String team;
@@ -24,5 +33,43 @@ public class ScoreEdit extends AppCompatActivity {
         team = b.getString("team");
         judge = b.getString("judge");
         //list of entry rows, with size equal to the size of labels, which is the list of subcategories retrieved.
+    }
+
+    //TODO add save button to end of list
+    public class EditAdapter extends BaseAdapter{
+        LayoutInflater inflater;
+        Context context;
+        public EditAdapter(Context context){
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            this.context = context;
+        }
+        @Override
+        public int getCount() {
+            return entries.size() + 1;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            if(i == entries.size())return null;//TODO save button
+            return entries.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+
+            Object o = getItem(i);
+            if(o instanceof Entry){
+                if (view == null) view = inflater.inflate(R.layout.score_edit_row, null);
+                return view;
+            } else{
+                return null;//Return the button, and add the listener to it.
+            }
+        }
     }
 }
